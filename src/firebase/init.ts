f99a899a6c686029fcdf15000297cf9093fbb72c
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-09-22 19:39:15 -0600

    hide sensative info

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-HSkdJpGjvq-gj0dslRxFsJjGoeyasa4",
-  authDomain: "makram-advanced-internship.firebaseapp.com",
-  projectId: "makram-advanced-internship",
-  storageBucket: "makram-advanced-internship.appspot.com",
-  messagingSenderId: "388608368477",
-  appId: "1:388608368477:web:1e8bbcd0185f41265752b4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
